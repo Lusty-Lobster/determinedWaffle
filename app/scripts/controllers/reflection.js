aa335b79d06cb59835629776fbd9a1b851cc6b2e
Katrina Uychaco <kuychaco@gmail.com>
2015-05-15 16:02:04 -0700

    Add progress bar functionality

--- app/scripts/controllers/reflection.js
+++ app/scripts/controllers/reflection.js
@@ -6,15 +6,53 @@ angular.module('thumbsCheckApp')
   // Show only one reflection at a time
   $scope.oneAtATime = true;
 
-  $scope.reflections = $firebaseArray(Ref.child('reflections'));
+  var reflections = $firebaseArray(Ref.child('reflections'));
   $scope.state = $firebaseObject(Ref.child('state'));
+
 
   $scope.curReflection;
   $scope.curTopics;
   $scope.curResponses;
 
+  var topicsRef; // = townHallsRef.child(stateObj.townHall).child('questions');
+  var topicsObj; // = $firebaseObject(topicsRef);
+
+  var convertToArray = function (reflections) {
+    return reflections.map(function (reflection) {
+      console.log(reflection.topics);
+      reflection.topics = reflection.topics || {};
+      reflection.topics = Object.keys(reflection.topics).map(function (topicId) {
+        return reflection.topics[topicId];
+      });
+      return reflection;
+    });
+  };
+
+
+  reflections.$loaded().then(function(reflections) {
+    $scope.reflections = convertToArray(reflections);
+
+    reflections.$watch(function() {
+      $scope.reflections = convertToArray(reflections);  
+    });
+  });
+
+
+
   $scope.state.$loaded().then(function(){
+
+    // console.log('questions obj', topicsObj);
     $scope.state.$watch(function(){
+      topicsRef = Ref.child('reflections').child($scope.state.reflection).child('topics');
+      topicsObj = $firebaseObject(topicsRef);
+
+      topicsObj.$loaded().then(function() {
+        $scope.total();
+        topicsObj.$watch(function() {
+          $scope.total();
+        });
+      });
+
       $scope.curReflection = $firebaseObject(
         Ref.child('reflections')
         .child($scope.state.reflection)
@@ -24,20 +62,6 @@ angular.module('thumbsCheckApp')
         .child($scope.state.reflection)
         .child('topics')
       );
-      $scope.curTopics.$loaded().then(function(){
-        $scope.curTopics.$watch(function(){
-          $scope.stackedResponses=[ ];
-          for( var i=0; i<$scope.curTopics.length; i++ ){
-            console.log('curtopics ', $scope.curTopics);
-            console.log('length ', $scope.curTopics[i].responses.length);
-            for( var k=0; k<$scope.curTopics[i].responses.length; k++){
-
-            }
-          }
-          console.log('changed', $scope.curResponses);
-        });
-      });
-      console.log($scope.curResponses);
     });
   })
 
@@ -77,4 +101,86 @@ angular.module('thumbsCheckApp')
       $scope.state.$save();
     });
   }
+
+  $scope.total = function() {
+    // Initialize quizCounts and studentList
+    // quizResponsesObj.$loaded().then(function(responses) {
+    //   tallyUpStudentResponsesService.tallyUpResponses(responses, undefined, studentList, quizCounts);
+    //   $scope.populateProgressBar(quizCounts);
+    //   $scope.studentList = studentList;
+    // });
+    // $scope.results = {};
+
+    // once townHallQuestionsObj is loaded, then
+    topicsObj.$loaded().then(function(topics) {
+      // loop - for each question
+      topics.forEach(function(topic, key) {
+        var thumbsCounts = [];
+        var studentList = {};
+        for (var i = 0; i < 3; i++) {
+          thumbsCounts.push(0);
+          studentList[i] = [];
+        }
+
+        var topicRef = topicsRef.child(key);
+        var topicObj = $firebaseObject(topicRef);
+
+        topicObj.$loaded().then(function(topic) {
+
+          topic.voteResult = tallyResponses(topic.responses, thumbsCounts);
+
+          topic.results = populateProgressBar(thumbsCounts);
+          topic.$save();
+        });
+      });
+    });
+  }; 
+
+  
+
+  var tallyResponses = function(responses, thumbsCounts) {
+    for (var student in responses) {
+      var vote = responses[student].vote;
+      if (vote === 'up') {
+        thumbsCounts[0]++;
+      } else if (vote === 'middle') {
+        thumbsCounts[1]++;
+      } else {
+        thumbsCounts[2]++;
+      }
+    }
+    var sum = thumbsCounts[0] + thumbsCounts[1] + thumbsCounts[2];
+
+    return (thumbsCounts[0] - thumbsCounts[2]) / sum;
+  };
+
+  // one progress bar for each question (question_id)
+  populateProgressBar = function(quizResult) {
+    stacked = [];
+
+    var types = ['success', 'info', 'warning', 'danger'];
+
+    var quizCountsTotal = quizResult.reduce(function(memo, x) {
+      return memo + x;
+    });
+
+    if (quizCountsTotal === 0) {
+      return stacked;
+    }
+
+    var choices = ['up', 'mid', 'down'];
+
+    quizResult.forEach(function(val, i) {
+      var percent = Math.floor((val / quizCountsTotal) * 100);
+      var type = types[i];
+      stacked.push({
+        value: percent,
+        type: type,
+        choice: choices[i]
+      });
+    });
+
+    return stacked;
+  };
+
 });
